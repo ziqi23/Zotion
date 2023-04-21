77f2d3b2995ee7fb04ade2f075be37861a3af91f
Ziqi Zou <ziqizou@MacBook-Air.local>
2023-04-21 13:50:48 -0700

    set up users table

---
+++ backend/app/models/user.rb
@@ -0,0 +1,42 @@
+class User < ApplicationRecord
+    has_secure_password
+    before_validation :ensure_session_token
+
+    validates :username, :email, :session_token, uniqueness:true
+    validates :username, length: { in: 3..30 }, format: { without: URI::MailTo::EMAIL_REGEXP, message: "can't be an email"}
+    validates :email, length: { in: 3..255 }, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :password, length: { in: 6..255 }, allow_nil: true
+    
+    def self.find_by_credentials(credential, password)
+        if (credential.match?(URI::MailTo::EMAIL_REGEXP))
+            user = User.find_by(email: credential)
+        else
+            user = User.find_by(username: credential)
+        end
+        if (user&.authenticate(password))
+            return user
+        else
+            return nil
+        end
+    end
+
+    private
+        
+    def ensure_session_token
+        self.session_token ||= generate_session_token
+    end
+
+    def reset_session_token!
+        self.session_token = generate_session_token
+        self.save!
+        return self.session_token
+    end
+
+    def generate_session_token
+        token = SecureRandom::urlsafe_base64
+        while (User.exists?(session_token: token))
+            token = SecureRandom::urlsafe_base64
+        end
+        return token;
+    end
+end
