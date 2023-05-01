4db3a2426daed1a50138bb811cc89a74db573c19
Ziqi Zou <brianzou1@gmail.com>
2023-05-01 14:38:11 -0700

    add cover switching

--- frontend/src/components/UserHomepage/Main/Title.js
+++ frontend/src/components/UserHomepage/Main/Title.js
@@ -15,6 +15,7 @@ const Title = (props) => {
     const dispatch = useDispatch()
     const location = useLocation()
     const [optionsVisible, setOptionsVisible] = useState(false)
+    const [addCoverVisible, setAddCoverVisible] = useState(false)
     const query = location.search;
     const pageId = query.slice(query.search("=") + 1, query.length)
     const pages = useSelector((state) => state.page)
@@ -35,6 +36,27 @@ const Title = (props) => {
     
     function handlePageNameChange(e) {
         dispatch(modifyPage({id: pageId, pageName: e.target.innerHTML}))
+    }
+
+    function handleAddCover(e) {
+        setAddCoverVisible(true)
+
+        function handleClick(e) {
+            const panel = window.document.getElementsByClassName('add-cover-panel')[0]
+            const rect = panel?.getBoundingClientRect();
+            const mouseX = e.clientX;
+            const mouseY = e.clientY;
+            if (rect && (mouseX < rect.left || mouseX > rect.right || mouseY < rect.top || mouseY > rect.bottom)) {
+                setTimeout(() => setAddCoverVisible(false), 0)
+                document.removeEventListener('mousedown', handleClick)
+            }
+        }
+        document.addEventListener("mousedown", handleClick)
+    }
+
+    function handleChooseCover(e) {
+        dispatch(modifyPage({id: pageId, pageIcon: e.currentTarget.getAttribute('data-url')}))
+        setAddCoverVisible(false)
     }
 
     const debounce = (func, wait) => {
@@ -64,18 +86,18 @@ const Title = (props) => {
           timeout = setTimeout(later, wait);
         };
     }
-    
+
     return (
         <>
         {pages[pageId]?.pageIcon && (
         <div className="main-title-cover">
-            <img src={[cover1, cover2, cover3, cover4, cover5][Math.floor(Math.random() * 5)]} draggable="false"></img>
+            <img src={`./assets/${pages[pageId].pageIcon}.jpeg`} draggable="false"></img>
         </div>
         )}
         <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
             {optionsVisible && (
             <div className="main-title-options">
-                <div className="add-cover">
+                <div className="add-cover" onClick={handleAddCover}>
                     Add cover
                 </div>
             </div>
@@ -87,6 +109,46 @@ const Title = (props) => {
             {pages[pageId] && (
                 <div className="main-title-text" contentEditable="true" suppressContentEditableWarning="true" onInput={debounce(handlePageNameChange, 500)}>
                     {pages[pageId].pageName}
+                </div>
+            )}
+            {addCoverVisible && (
+                <div className="add-cover-panel">
+                    <div className="individual-cover">
+                        <img data-url="rijksmuseum_avercamp_1608" src='./assets/rijksmuseum_avercamp_1608.jpeg' draggable="false" onClick={handleChooseCover}></img>
+                    </div>
+                    <div className="individual-cover">
+                        <img data-url="rijksmuseum_claesz_1628" src='./assets/rijksmuseum_claesz_1628.jpeg' draggable="false" onClick={handleChooseCover}></img>
+                        </div>
+                    <div className="individual-cover">
+                        <img data-url="rijksmuseum_jan_lievens_1627" src='./assets/rijksmuseum_jan_lievens_1627.jpeg' draggable="false" onClick={handleChooseCover}></img>
+                    </div>
+                    <div className="individual-cover">
+                        <img data-url="rijksmuseum_mignons_1660" src='./assets/rijksmuseum_mignons_1660.jpeg' draggable="false" onClick={handleChooseCover}></img>
+                    </div>
+                    <div className="individual-cover">
+                        <img data-url="rijksmuseum_rembrandt_1642" src='./assets/rijksmuseum_rembrandt_1642.jpeg' draggable="false" onClick={handleChooseCover}></img>
+                    </div>
+                    <div className="individual-cover">
+                        <img data-url="rijksmuseum_milkmaid" src='./assets/rijksmuseum_vermeer_the_milkmaid.jpeg' draggable="false" onClick={handleChooseCover}></img>
+                    </div>
+                    <div className="individual-cover">
+                        <img data-url="woodcuts_1" src='./assets/woodcuts_1.jpeg' draggable="false" onClick={handleChooseCover}></img>
+                    </div>
+                    <div className="individual-cover">
+                        <img data-url="woodcuts_2" src='./assets/woodcuts_2.jpeg' draggable="false" onClick={handleChooseCover}></img>
+                    </div>
+                    <div className="individual-cover">
+                        <img data-url="woodcuts_5" src='./assets/woodcuts_5.jpeg' draggable="false" onClick={handleChooseCover}></img>
+                    </div>
+                    <div className="individual-cover">
+                        <img data-url="woodcuts_6" src='./assets/woodcuts_6.jpeg' draggable="false" onClick={handleChooseCover}></img>
+                    </div>
+                    <div className="individual-cover">
+                        <img data-url="woodcuts_13" src='./assets/woodcuts_13.jpeg' draggable="false" onClick={handleChooseCover}></img>
+                    </div>
+                    <div className="individual-cover">
+                        <img data-url="woodcuts_14" src='./assets/woodcuts_14.jpeg' draggable="false" onClick={handleChooseCover}></img>
+                    </div>
                 </div>
             )}
         </div>
